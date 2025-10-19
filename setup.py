049a642a283b4be3ce15d0f8a92a62eda2ba636d
Dylan Murray <211373951+DylanEdM@users.noreply.github.com>
2025-10-19 02:56:16 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     # set up for the program
     subprocess.run("python -m venv .\\.venv")
     # activate the python environment
-    system('''.\\.venv\\Scripts\\activate && python -m pip install -r .\\requirements.txt''')
+    system('''.\\.venv\\Scripts\\activate && python -m pip install -r .\\requirements.txt && python main.py''')
